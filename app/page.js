a511a082388949f273ca5bd87d0aaaf33b5507ad
cytraark <rayghanimadhizky@gmail.com>
2023-12-12 17:45:45 +0700

    Clean the User Interface

--- app/page.js
+++ app/page.js
@@ -1,17 +1,36 @@
 'use client'
-import { Table, Input, Button } from "rsuite"
+import { Table, Input, Button,IconButton } from "rsuite"
 import { useState, useEffect } from "react"
 import axios from "axios"
+import PauseIcon from '@rsuite/icons/legacy/Pause';
+import PlayIcon from '@rsuite/icons/legacy/Play';
 
 export default function Home() {
   const [data, setData] = useState([])
-  const [filterMusic,setFilterMusic] = useState([])
-  const [searchState, setSearchState] = useState()
+  const [searchState, setSearchState] = useState("")
+  const [play, setPlay] = useState(false)
+  const handlePlay = () => setPlay(true)
+  const handlePause = () => setPlay(false)
+  const mainButton = new Audio()
   const { Column, HeaderCell, Cell } = Table;
+  const getMusic =  async() => { 
+      const options = {
+        method: 'GET',
+        url: 'https://deezerdevs-deezer.p.rapidapi.com/search',
+        params: {q: searchState},
+        headers: {
+          'X-RapidAPI-Key': 'f78ffe324fmshed767f7d95c68d1p1f5856jsneb2c7255ab35',
+          'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+        }
+      };
+      let access = await axios.request(options)
+      console.log(access.data.data)
+      setData(access.data.data)
+    }
+    getMusic()
 
   useEffect(() => { 
     const getMusic = async () => { 
-      let array = []
       const options = {
         method: 'GET',
         url: 'https://deezerdevs-deezer.p.rapidapi.com/search',
@@ -22,49 +41,58 @@ export default function Home() {
         }
       };
       let access = await axios.request(options)
-    
+      console.log(access.data.data)
       setData(access.data.data)
     }
     getMusic()
-  },[])
+  }, [])
+  
   return (
     <>
       <div className=" w-screen h-screen flex flex-col gap-5 bg-white">
         <div className="flex flex-col place-content-center pt-10">
-          <div className="flex flex-row justify-start pl-10 pt-5 text-black">
+          <div className="flex flex-row justify-center pl-10 pt-5 text-black">
             <h2>Music App for Testing</h2>
           </div>
-           <div className="flex flex-row justify-start gap-2 pt-10 pl-10 pr-10 w-1/2">
-            {" "}
+          <div className="flex flex-col justify-center place-content-center gap-2 pt-10 pl-40 pr-40">
             <Input
               size="md"
               placeholder="Search Music!"
-              onChange={setSearchState}
+              onChange={() => { 
+                setSearchState
+              }}
               value={searchState}
             />
+            <Button appearance="ghost" color="green" onClick={async() => { 
+              getMusic()
+            }}>Search</Button>
           </div>
-          <div className="flex flex-col place-content-center justify-center">
+          <div className="flex flex-wrap place-content-center justify-center">
             {data === undefined || data === null ? (
               <>
                 <div className="flex flex-row gap-2 pt-10 justify-center w-1/2">
-                  <p>There is no data. Please refresh!</p>
+                  <p>There is no music. Please search!</p>
                 </div>
               </>
             ) : (<>
-              <Table
-               style={{
-                      display: "block",
-                      width: 950,
-                      height: 500,
-                      padding: 40,
-                      justifyContent: "center",
-                  }}
-              virtualized
-              data={data}
-              onRowClick={rowData => {
-                console.log(rowData);
-              }}
-            >
+                <div className="flex flex-col justify-center gap-3">
+
+                  <Table
+                    style={{
+                            display: "block",
+                            width: 950,
+                            padding: 40,
+                            justifyContent: "center",
+                    }}
+                    height={500}
+                    virtualized
+                    data={data}
+                    onRowClick={rowData => {
+                      {
+                        mainButton(rowData.preview)
+                      }
+                    }}
+                >
               <Column width={400}>
                 <HeaderCell>Song</HeaderCell>
                 <Cell dataKey="title" />
@@ -85,14 +113,33 @@ export default function Home() {
 
                 <Cell style={{ padding: '6px' }}>
                   {rowData => (
-                    <Button appearance="ghost" onClick={() => alert(`id:${rowData.id}`)}>
-                      Play
+                        <Button appearance="ghost" onClick={() => { 
+                          var audio = new Audio(rowData.preview)
+                          if (play == true) {
+                            handlePause()
+                            audio.pause()
+                          } else { 
+                            handlePlay()
+                            audio.play()
+                          }
+                        }}>
+                          {play ? "Pause":"Play"}
                     </Button>
                   )}
                 </Cell>
               </Column>
             </Table>
+                </div>
+              
             </>)}
+          </div>
+          <div className="flex flex-row place-content-center gap-3 justify-center">
+              <IconButton icon={<PauseIcon />} placement="left">
+                Pause
+              </IconButton>
+              <IconButton icon={<PlayIcon />} placement="right">
+                Play
+              </IconButton>
           </div>
         </div> 
       </div>
